c84e77e841b1f19520b25db59fa8790e46563eec
moses ngige <moses32ngigz@gmail.com>
2022-06-12 01:40:49 +0300

    commit tree 35e9ca879dfcb980b137f589530ff0626f1053de

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Follow along with the examples here
+function doNothing() {}
+function sayHello() {
+    console.log("Hello!");
+  }
+sayHello();
+function sayHelloToGuadalupe() {
+    console.log("Hello, Guadalupe!");
+}
+sayHelloToGuadalupe()
+function sayHelloToLiz() {
+    console.log("Hello, Liz!");
+}
+sayHelloToLiz()
+function sayHelloToSamip() {
+    console.log("Hello, Samip!");
+  }
+  sayHelloToSamip()
+function say(greeting, firstName) {
+    console.log(`${greeting}, ${firstName}!`);
+  }
+  say("Goodbye", "Julio");
+function say(greeting, firstName) {
+    return `${greeting}, ${firstName}!`;
+    console.log("I was called!");
+  }
+  console.log(say("Howdy", "partner"));
+function moses(){
+    console.log("i am Moses");
+  }
+moses();
+function say(greeting, firstName) {
+    return `${greeting}, ${firstName}!`;
+  }
+  say("Hello", "Liz");
